adc60b5d72b636685653b020d4b474f91f0a32fd
Sameera Pujari <166124851+sameerapujari@users.noreply.github.com>
2026-01-30 23:02:36 +0530

    Time: 5 ms (60.74%), Space: 83.1 MB (30.79%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,20 +1,37 @@
 class Solution {
     public int findDuplicate(int[] nums) {
+
         //binary search on nums range-- 1 to n only
         //pigeonhole principle
-        int l=1, h=nums.length;
+        //tc - O(nlogn) sc - O(1)
+        // int l=1, h=nums.length;
         
-        while(l<h){
-            int mid = l+(h-l)/2;
-            int count=0;  //keep a count of ele < mid to narrow search space
-            for(int x:nums){
-                if(x<=mid)count++;
-            }
-            if(count>mid){
-                h=mid;
-            }
-             else l = mid+1;
+        // while(l<h){
+        //     int mid = l+(h-l)/2;
+        //     int count=0;  //keep a count of ele < mid to narrow search space
+        //     for(int x:nums){
+        //         if(x<=mid)count++;
+        //     }
+        //     if(count>mid){
+        //         h=mid;
+        //     }
+        //      else l = mid+1;
+        // }
+        // return l;
+
+        //floyds cycle detection solution
+        //tc-O(n) sc-O(1)
+        int slow=nums[0], fast=nums[0];
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow!=fast);
+        
+        slow=nums[0];
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
         }
-        return l;
+        return slow;
     }
 }
