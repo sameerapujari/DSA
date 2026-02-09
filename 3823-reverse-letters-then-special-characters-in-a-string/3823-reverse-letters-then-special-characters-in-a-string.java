174bb52a18b1b9c778fe08be249ec1605b9f5585
Sameera Pujari <166124851+sameerapujari@users.noreply.github.com>
2026-02-09 22:09:45 +0530

    Time: 3 ms (57.68%), Space: 44.9 MB (56.17%) - LeetHub

--- 3823-reverse-letters-then-special-characters-in-a-string/3823-reverse-letters-then-special-characters-in-a-string.java
+++ 3823-reverse-letters-then-special-characters-in-a-string/3823-reverse-letters-then-special-characters-in-a-string.java
@@ -1,19 +1,39 @@
 class Solution {
     public String reverseByType(String s) {
+        // StringBuilder chs = new StringBuilder();
+        // StringBuilder spl = new StringBuilder();
+        // StringBuilder ans = new StringBuilder();
+        // for(char c: s.toCharArray()){
+        //     if(Character.isLetter(c)) chs.append(c);
+        //     else spl.append(c);
+        // }
+        // chs.reverse();
+        // spl.reverse();
+        // int i=0,j=0;
+        // for(char c:s.toCharArray()){
+        //     if(Character.isLetter(c)) {
+        //         ans.append(chs.charAt(i)); i++;}
+        //     else {ans.append(spl.charAt(j)); j++;}
+        // }
+        // return ans.toString();
+
+
+        //w/o rev use Character.isLetter
+        StringBuilder ans = new StringBuilder();
         StringBuilder chs = new StringBuilder();
         StringBuilder spl = new StringBuilder();
-        StringBuilder ans = new StringBuilder();
-        for(char c: s.toCharArray()){
-            if(Character.isLetter(c)) chs.append(c);
-            else spl.append(c);
+
+        for (char c : s.toCharArray()) {
+            if (Character.isLetter(c)) {
+                chs.append(c);
+            } else {
+                spl.append(c);
+            }
         }
-        chs.reverse();
-        spl.reverse();
-        int i=0,j=0;
+        int i=chs.length()-1, j=spl.length()-1;
         for(char c:s.toCharArray()){
-            if(Character.isLetter(c)) {
-                ans.append(chs.charAt(i)); i++;}
-            else {ans.append(spl.charAt(j)); j++;}
+            if(Character.isLetter(c))ans.append(chs.charAt(i--));
+            else ans.append(spl.charAt(j--));
         }
         return ans.toString();
     }
