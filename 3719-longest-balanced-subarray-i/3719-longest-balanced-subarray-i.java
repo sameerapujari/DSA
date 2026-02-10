6fd616eed69149f37f5233a54b4fd3ed1f3dab7d
Sameera Pujari <166124851+sameerapujari@users.noreply.github.com>
2026-02-10 22:00:04 +0530

    Time: 299 ms (77.55%), Space: 47 MB (68.61%) - LeetHub

---
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int longestBalanced(int[] nums) {
+        //distinct numbers
+        // maintain subarr till eve-odd=0 diff: distinct and eql cnt so far
+        int n=nums.length;
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            HashSet<Integer> even = new HashSet<>();
+            HashSet<Integer> odd = new HashSet<>();
+
+           for(int j=i; j<n; j++){
+            if(nums[j]%2==0) even.add(nums[j]);
+            else odd.add(nums[j]);
+
+            if(even.size()==odd.size()) ans = Math.max(ans,j-i+1);  //length of subarr so far
+           }
+        }
+        return ans;
+    }
+}
