19050a46dd46161b25bcfdfd89ae9c3aafaf15b4
Sameera Pujari <166124851+sameerapujari@users.noreply.github.com>
2026-03-11 23:21:21 +0530

    Time: 0 ms (100%), Space: 42.3 MB (50.09%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -1,13 +1,21 @@
 class Solution {
     public int bitwiseComplement(int n) {
-        if (n==0) return 1;
-        String bin = Integer.toBinaryString(n);
-        StringBuilder sb = new StringBuilder();
-        for(char c:bin.toCharArray()){
-            if(c=='0')sb.append(1);
-            else sb.append(0);
+        // if (n==0) return 1;
+        // String bin = Integer.toBinaryString(n);
+        // StringBuilder sb = new StringBuilder();
+        // for(char c:bin.toCharArray()){
+        //     if(c=='0')sb.append(1);
+        //     else sb.append(0);
+        // }
+        // int ans = Integer.parseInt(sb.toString(),2);
+        // return ans;
+        //optimal: xor ^1
+        //mask = bits + flipped bits
+        if(n==0) return 1;
+        int mask=1;
+        while(mask<=n){
+            mask=mask<<1;
         }
-        int ans = Integer.parseInt(sb.toString(),2);
-        return ans;
+        return (mask-1)^n;
     }
 }
