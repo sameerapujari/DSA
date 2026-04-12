01ecdd023459143b731ddd95561796cc9c8447d6
Sameera Pujari <166124851+sameerapujari@users.noreply.github.com>
2026-04-12 22:37:30 +0530

    Time: 0 ms (100%), Space: 42 MB (77.48%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+        //fibonacci pattern
+        if(n<=1)return 1;
+        int prev1 = 1, prev2 = 1, curr=0;
+        for(int i=2; i<=n; i++){
+            curr = prev1+prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return curr;
+    }
+}
