134c7f4d74650aa20b3ce1fd41ec09dc9a24dfea
Sameera Pujari <166124851+sameerapujari@users.noreply.github.com>
2026-05-26 12:02:19 +0530

    Time: 93 ms (3.81%), Space: 43.8 MB (46.29%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
@@ -1,19 +1,32 @@
 class Solution {
     public int numberOfSpecialChars(String word) {
-        //use sets
-        HashSet<Character> lower = new HashSet<>();
-        HashSet<Character> upper = new HashSet<>();
-        int count=0;
+//         //use sets
+//         HashSet<Character> lower = new HashSet<>();
+//         HashSet<Character> upper = new HashSet<>();
+//         int count=0;
 
-//duplicates gone
-        for(int i=0; i<word.length(); i++){
-            char c = word.charAt(i);
-            if(Character.isLowerCase(c)) lower.add(c);
-            else upper.add(Character.toLowerCase(c));
-        }
-        for(char c: lower){
-            if(upper.contains(c))count++;
-        }
-        return count;
+// //duplicates gone
+//         for(int i=0; i<word.length(); i++){
+//             char c = word.charAt(i);
+//             if(Character.isLowerCase(c)) lower.add(c);
+//             else upper.add(Character.toLowerCase(c));
+//         }
+//         for(char c: lower){
+//             if(upper.contains(c))count++;
+//         }
+//         return count;
+
+            boolean[] lower = new boolean[26];
+            boolean[] upper = new boolean[26];
+
+            for(char c: word.toCharArray()){
+                if(Character.isLowerCase(c)) lower[c-'a'] = true;
+                else upper[c-'A'] = true;
+            }
+            int count=0;
+            for(int i=0; i<26; i++){
+                if(lower[i] && upper[i])count++;
+            }
+            return count;
     }
 }
