7a3331a1529ef0c46120cac2ab2e1682a3671f35
Sameera Pujari <166124851+sameerapujari@users.noreply.github.com>
2026-06-19 23:04:22 +0530

    Time: 0 ms (100%), Space: 43.7 MB (89.61%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,11 +1,27 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(matrix[i][j] == target) return true;
-            }
+        //lol this q is tagged for BS
+
+        // int m = matrix.length;
+        // int n = matrix[0].length;
+        // for(int i=0; i<m; i++){
+        //     for(int j=0; j<n; j++){
+        //         if(matrix[i][j] == target) return true;
+        //     }
+        // }
+        // return false;
+
+        //BS
+        //flatten out the matrix then fig out mat index to arr index mapping
+        int m = matrix.length, n=matrix[0].length;
+        int l=0, h=(m*n)-1;
+
+        while(l<=h){
+            int mid = l + (h-l)/2;
+            int val = matrix[mid/n][mid%n]; // most impppp
+            if(val==target) return true;
+            else if(val>target) h=mid-1;
+            else l=mid+1;
         }
         return false;
     }
