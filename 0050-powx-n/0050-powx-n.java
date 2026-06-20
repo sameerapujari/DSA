96d668f459288758d75298d5e15a0d2babea335e
Sameera Pujari <166124851+sameerapujari@users.noreply.github.com>
2026-06-20 14:43:42 +0530

    Time: 0 ms (100%), Space: 48 MB (56.57%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,18 +1,22 @@
 class Solution {
-    public double myPow(double x, int n) {
-        long N = n;
-        if (N < 0) {
-            x = 1 / x;
-            N = -N;
+    public double myPow(double x, int N) {
+        //now most imp edge case when u conv last INT_MIN to +ve its int overflow
+        //hence use long
+        //neg expo
+
+        long n = N;
+        if(n<0) return 1.0/calcPow(x,-n);
+        return calcPow(x,n);
+    }
+    private double calcPow(double x, long n){
+        //will use recursion d&c approach
+        //base case
+        if(n==0) return 1.0;
+        if(n==1) return x;
+
+        if(n%2==0){  //even power
+            return calcPow(x*x,n/2);
         }
-        double ans = 1.0;
-        while (N > 0) {
-            if ((N & 1) == 1) {
-                ans *= x;
-            }
-            x *= x;
-            N >>= 1;
-        }
-        return ans;
+        return x*calcPow(x,n-1);  //odd power
     }
 }
