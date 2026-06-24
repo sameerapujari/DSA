d16c8e131ccadf5fe95387522c562e8ddd1bd1f6
Sameera Pujari <166124851+sameerapujari@users.noreply.github.com>
2026-06-24 18:21:44 +0530

    Time: 0 ms (100%), Space: 41.6 MB (97.75%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        //recursively generate all paths -- tle tc:2^(m+n)
+        //optimal -- dp tc: O(m+n)
+        int[][] dp = new int[m][n];
+
+        for(int i=0; i<m; i++) dp[i][0] = 1;
+        for(int j=0; j<n; j++) dp[0][j] = 1;
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                //memoization
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
